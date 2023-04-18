f61c5e80751370c0014cbb5c5ef98c616eb000e6
Thomas Jensen <33011576+hrimfisk@users.noreply.github.com>
2023-04-17 19:29:46 -0700

    Removing Source Code
    This should be screenshots, not code

--- SaveGameLibrary/Source/SaveGameLibrary/Private/Utils/LogDeclarations.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Thomas Howard Jensen published 2022
-
-/*=============================================================================
-	LogDeclarations.cpp: Definition macros for log categories
-=============================================================================*/
-
-#include "LogDeclarations.h"
-
-DEFINE_LOG_CATEGORY(LogSaveGame);
